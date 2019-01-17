b4ebc6f07e36e2090bc18d5448987a71776de51e
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2019-01-17 21:58:46 +0300

    get_uid shortened

--- Python/pmdeck/get_uid.py
+++ Python/pmdeck/get_uid.py
@@ -1,10 +1,24 @@
 
 import netifaces
 import hashlib
+from baseconv import base16
+from baseconv import BaseConverter
+
 
 def get_uid():
     interfaces = netifaces.interfaces()
     interfaces.sort()
-    hashid = hashlib.md5(interfaces[0].encode("utf-8")).hexdigest()
-    uid = "Python-{}".format(hashid)
+    i = interfaces[0]
+    s = "abcdefghijklmnopqrstuvwxyz"
+    u = "ABCDEFGHJKLMNPQRSTUVWXYZ"
+    d = "0123456789"
+    bc = BaseConverter(d+s+u)
+    hashid = hashlib.sha1(i.encode("utf-8")).hexdigest()
+    hashid = hashid.upper()
+    raw = base16.decode(hashid)
+    uid = bc.encode(raw)
     return uid
+
+
+def get_short_uid():
+    return get_uid()[:9]
