0998101f26a3d9d243eb0fdb0d93e8afa4c42cfc
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2019-01-20 13:31:02 +0300

    folder.py refactor

--- Python/Action/folder.py
+++ Python/Action/folder.py
@@ -1,9 +1,10 @@
 
 from Action.action import Action
 
+
 class Folder:
 
-    def __init__(self,deck):
+    def __init__(self, deck):
         
         # List of Actions
         self.actions = [Action(deck)] * 15
@@ -13,10 +14,9 @@ class Folder:
 
     def set_action(self, space_index:int, action):
         if not 14 >= space_index >= 0:
-            print("Space index error: "+ space_index)
+            print("Space index error: " + space_index)
             return
         self.actions[space_index] = action
-
 
     def open(self):
 
@@ -29,13 +29,12 @@ class Folder:
         self.deck.current_folder = self
         return
 
-
     def button_pressed(self, space_index:int):
         if self.actions[int(space_index)]:
             self.actions[int(space_index)]._pressed()
 
-
     def button_released(self, space_index:int):
         if self.actions[int(space_index)]:
             self.actions[int(space_index)]._released()
+
 
